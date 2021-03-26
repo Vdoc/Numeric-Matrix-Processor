da732b280d2d11d83f575ad223d7fef41aba7b1b
NikoBolt <docnikbolt@gmail.com>
2021-03-26 03:06:53 +0300

    Stage 4-6 Transpose

--- Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric Matrix Processor/task/src/processor/Main.kt
@@ -1,159 +1,92 @@
 package processor
 
-import java.util.*
+data class Matrix(val m: Int, val n: Int, val matrix: List<List<Double>>)
 
-val scanner = Scanner(System.`in`)
+fun addition() {
+    val (mA, nA, a) = getMatrix(" first")
+    val (mB, nB, b) = getMatrix(" second")
+    println("The result is:")
+    if (mA to nA == mB to nB) List(mA as Int) { a[it].zip(b[it]) { x, y -> x + y } }.printMatrix()
+    else println("The operation cannot be performed.")
+}
+
+fun multiplyConst() {
+    val (_, n, a) = getMatrix("")
+    print("Enter constant: ")
+    val c = readLine()!!.toInt()
+    println("The result is:")
+    a.flatten()
+            .map { it * c }
+            .chunked(n)
+            .printMatrix()
+}
+
+fun multiplyMatrices() {
+    val (mA, nA, a) = getMatrix(" first")
+    val (mB, nB, b) = getMatrix(" second")
+    println("The result is:")
+    val c = List(mA) { MutableList(nB) { 0.0 } }
+    if (nA == mB) {
+        for (i in 0 until mA)
+            for (j in 0 until nB)
+                for (k in 0 until nA)
+                    c[i][j] += a[i][k] * b[k][j]
+        c.printMatrix()
+    } else println("The operation cannot be performed.")
+}
+
+fun transposeMatrix() {
+    println("\n1. Main diagonal\n2. Side diagonal\n3. Vertical line\n4. Horizontal line\nYour choice: ")
+    val choice = readLine()!!.toInt()
+    val (m, n, a) = getMatrix("")
+    when (choice) {
+        1 -> {
+            val b = List(n) { MutableList(m) { 0.0 } }
+            a.forEachIndexed { i, v -> v.forEachIndexed { j, x -> b[j][i] = x } }
+            b.printMatrix()
+        }
+        2 -> {
+            val b = List(n) { MutableList(m) { 0.0 } }
+            a.forEachIndexed { i, v -> v.forEachIndexed { j, x -> b[n - j - 1][m - i - 1] = x } }
+            b.printMatrix()
+        }
+        3 -> {
+            val b = List(m) { MutableList(n) { 0.0 } }
+            a.forEachIndexed { i, v -> v.forEachIndexed { j, x -> b[i][n - j - 1] = x } }
+            b.printMatrix()
+        }
+        4 -> {
+            val b = List(m) { MutableList(n) { 0.0 } }
+            a.forEachIndexed { i, v -> v.forEachIndexed { j, x -> b[m - i - 1][j] = x } }
+            b.printMatrix()
+        }
+    }
+}
+
+fun getMatrix(name: String): Matrix {
+    print("Enter size of$name matrix: ")
+    val (m, n) = readLine()!!.split(" ").map(String::toInt)
+    println("Enter$name matrix: ")
+    val a = List(m) { readLine()!!.split(" ").map(String::toDouble) }
+    return Matrix(m, n, a)
+}
+
+fun List<List<Double>>.printMatrix() = joinToString("\n") {
+    it.joinToString(" ") {
+        if (it % 1.0 == 0.0) it.toInt().toString() else it.toString()
+    }
+}.let(::println)
 
 fun main() {
-    val np = NumericProcessor()
-    while (!np.exit) {
-        np.menu()
-    }
-}
-
-class NumericProcessor {
-    var exit = false
-    fun menu() {
-        print(  "1. Add matrices\n" +
-                "2. Multiply matrix by a constant\n" +
-                "3. Multiply matrices\n" +
-                "0. Exit\n" +
-                "Your choice: ")
-        val answer = scanner.nextInt()
-        try {
-            val matrix: Matrix = when (answer) {
-                1 -> createMatrix(1).add(createMatrix(2))
-                2 -> createMatrix().multiply()
-                3 -> createMatrix(1).multiply(createMatrix(2))
-                else -> {
-                    exit = true
-                    return
-                }
-            }
-            println("The result is:")
-            matrix.print()
-            println()
-
-        } catch (e: Error) {
-            println("The operation cannot be performed.\n")
+    while (true) {
+        print("1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n4. Transpose matrix\n0. Exit\nYour choice: ")
+        when (readLine()!!.toInt()) {
+            1 -> addition()
+            2 -> multiplyConst()
+            3 -> multiplyMatrices()
+            4 -> transposeMatrix()
+            0 -> return
         }
-    }
-
-    private fun createMatrix(count: Int = 0): Matrix {
-        var word = ""
-        when (count) {
-            1 -> word = " first"
-            2 -> word = " second"
-        }
-        print("Enter size of$word matrix: ")
-        val a = scanner.nextInt()
-        val b = scanner.nextInt()
-        print("Enter$word matrix:\n")
-
-        return Matrix(a, b, true)
-    }
-}
-
-class Matrix {
-    val width: Int
-    val height: Int
-    var matrix: Array<DoubleArray> = emptyArray()
-    var lines: Array<String> = emptyArray()
-    var line: String = ""
-    var number: Double = 0.0
-    var isDoublesHere: Boolean
-
-    constructor (rows: Int, columns: Int, usersInput: Boolean = false, doublesHere: Boolean = false) {
-        width = columns
-        height = rows
-        matrix = Array(height) { DoubleArray(width) }
-        isDoublesHere = doublesHere
-
-        if (usersInput) {
-            while (lines.size != height) {
-                line = scanner.nextLine()
-                if (line == "") continue
-                if (line.contains('.')) isDoublesHere = true
-                lines += line
-            }
-
-            for (_Y in 0 until height) {
-                line = lines[_Y]
-                val elements = line.split(" ")
-                for (_X in 0 until width) {
-                    if (elements[_X] == "") continue
-                    setElement(_Y, _X, elements[_X].toDouble())
-                }
-            }
-        }
-
-    }
-
-    fun getElenent(row: Int, column: Int): Double = matrix[row][column]
-
-    fun setElement(row: Int, column: Int, nomber: Double) {
-        matrix[row][column] = nomber
-    }
-
-    fun add(otherMatrix: Matrix): Matrix {
-        if (width != otherMatrix.width || height != otherMatrix.height ) throw Error()
-
-        var newMatrix: Matrix = Matrix(height, width,false, isDoublesHere)
-        for (_Y in 0 until height) {
-            for (_X in 0 until width) {
-                number = getElenent(_Y, _X) + otherMatrix.getElenent(_Y, _X)
-                newMatrix.setElement(_Y, _X, number)
-            }
-        }
-        return newMatrix
-    }
-
-    fun multiply(): Matrix {
-        println("Enter constant: ")
-        val constant = scanner.nextLine().toDouble()
-
-        var newMatrix: Matrix = Matrix(height, width, false, isDoublesHere)
-        for (_Y in 0 until height) {
-            for (_X in 0 until width) {
-                number = getElenent(_Y, _X) * constant
-                newMatrix.setElement(_Y, _X, number)
-            }
-        }
-        return newMatrix
-    }
-
-    fun multiply(otherMatrix: Matrix): Matrix {
-        if (width != otherMatrix.height )  throw Error()
-
-        val newMatrix: Matrix = Matrix(height, otherMatrix.width, false, isDoublesHere)
-        for (_Y in 0 until height) {
-            for (_X in 0 until otherMatrix.width) {
-                number = multiplyElements(_Y, _X, otherMatrix)
-                newMatrix.setElement(_Y, _X, number)
-            }
-        }
-        return newMatrix
-    }
-
-    private fun multiplyElements(_Y: Int, _X: Int, otherMatrix: Matrix): Double {
-        var result: Double = 0.0
-        for (i in 0 until width) {
-            result += getElenent(_Y, i) * otherMatrix.getElenent(i, _X)
-        }
-        return result
-    }
-
-    fun print() {
-        var output = ""
-        for (_Y in 0 until height) {
-            for (_X in 0 until width) {
-                if (isDoublesHere)
-                    output += "" + getElenent(_Y, _X) + " "
-                else
-                    output += "" + getElenent(_Y, _X).toInt() + " "
-            }
-            output += "\n"
-        }
-        print(output)
+        println()
     }
 }
